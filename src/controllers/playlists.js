78e7f84aa7598860cbb687e4d291ce05f87641b5
Gabriel Petry <gabrielpetry@outlook.com>
2019-09-20 11:35:25 -0300

    Playlists controller

---
+++ src/controllers/playlists.js
@@ -0,0 +1,12 @@
+const { api } = require('../services/api');
+
+module.exports = {
+  getPlaylists: async () => {
+    let result = await api.get('/playlists/songs')
+      .catch(err => console.log(err));
+    if (!result)
+      return [];
+    return result.data.playlists;
+
+  }
+};
