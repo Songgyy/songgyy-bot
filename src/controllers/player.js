0a883e1a0ccec9266d74491b04b3335b9817dcbf
Gabriel Petry <gabrielpetry@outlook.com>
2019-09-20 11:35:11 -0300

    Player controller for managing songs

---
+++ src/controllers/player.js
@@ -0,0 +1,59 @@
+const ytdl = require('ytdl-core');
+
+module.exports = {
+  play: async (sq, message) => {
+
+    const song = sq.songs[0];
+
+    if (!song) {
+      sq.textChannel.send("Nothing else to play!");
+      sq.playing = false;
+      return false;
+    }
+
+    let next_song = sq.songs[1];
+
+    let current_song_info = await ytdl.getInfo(song.youtube_link);
+    let next_song_info = null;
+
+    if(next_song)
+      next_song_info = await ytdl.getInfo(next_song.youtube_link);
+
+    let playing_status = [`Playing **${current_song_info.title}**`];
+    if (next_song_info)
+      playing_status.push(`Next song **${next_song_info.title}**`);
+    else
+      playing_status.push(`No more songs in the playlist`);
+
+    sq.textChannel.send(`${playing_status.map(s => `${s}\n`)}`);
+    sq.playing = true;
+
+    let ytdl_option = { filter: 'audioonly' };
+    let playStream_option = { type: 'opus' };
+    const dispatcher = sq.connection
+      .playStream(ytdl(song.youtube_link, ytdl_option), playStream_option)
+      .on('end', () => {
+        sq.songs.shift();
+        if(sq.songs.length < 1) {
+          sq.playing = false;
+          sq.textChannel.send("Playlist ended!");
+          return;
+        }
+        module.exports.play(sq, message);
+      })
+      .on('error', error => console.log(error));
+  },
+  canPlay: async (sq, message) => {
+    if (!sq) {
+      await message.channel.send("Please, use join first");
+      return false;
+    }
+    if (!sq.connection) {
+      await sq.textChannel.send("Your are not in a voice room");
+      return false;
+    }
+
+    return true;
+    
+  }
+};
