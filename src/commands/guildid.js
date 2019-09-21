6a901df298b87fcb077b4e2f9ea17cdb8c156046
Gabriel Petry <gabrielpetry@outlook.com>
2019-09-21 11:03:42 -0300

    Fixed typo

--- src/commands/guildid.js
+++ src/commands/guildid.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
 
     run: async (client, message, args) => {
-        await message.send(`Your guild id is **${message.guild.id}**`);
+        await message.channel.send(`Your guild id is **${message.guild.id}**`);
     },
 
     help: {
