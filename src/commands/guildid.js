5056f190e44190a02df35c968291fd0354ec34ed
Gabriel Petry <gabrielpetry@outlook.com>
2019-09-21 10:56:51 -0300

    Added command to show the current guild id

---
+++ src/commands/guildid.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports = {
+
+    run: async (client, message, args) => {
+        await message.send(`Your guild id is **${message.guild.id}**`);
+    },
+
+    help: {
+        name: "guildid",
+        alias: [ "guild" ],
+        description: "Show the guild id!",
+        usage: "guildid"
+    }
+};
