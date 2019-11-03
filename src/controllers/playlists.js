15843c3f6d493c25ba8f27c696bdb572b4760003
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 14:18:02 -0300

    prettier

--- src/controllers/playlists.js
+++ src/controllers/playlists.js
@@ -1,11 +1,11 @@
-const { api } = require('../services/api');
+const { api } = require("../services/api");
 
 module.exports = {
   getPlaylists: async () => {
-    let result = await api.get('/playlists/songs')
+    let result = await api
+      .get("/playlists/songs")
       .catch(err => console.log(err));
-    if (!result)
-      return [];
+    if (!result) return [];
     return result.data.playlists;
   }
 };
