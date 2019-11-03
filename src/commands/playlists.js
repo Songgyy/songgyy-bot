40a9f35e28d1ad17d84431a52e39401d580b9f93
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 14:51:25 -0300

    print the amoutn of songs

--- src/commands/playlists.js
+++ src/commands/playlists.js
@@ -1,37 +1,36 @@
 const Discord = require("discord.js");
-const Playlists = require('../controllers/playlists');
+const Playlists = require("../controllers/playlists");
 
 module.exports = {
+  run: async (client, message, args) => {
+    let playlists = await Playlists.getPlaylists();
 
-    run: async (client, message, args) => {
-        let playlists = await Playlists.getPlaylists();
+    let embed = {
+      color: client.theme.color,
+      title: "Command list for Songyy-bot",
+      url: "https://github.com/gabrielpetry/songyy-bot",
+      description: "All available commands",
+      footer: {
+        text: "Songyy-Bot"
+      },
+      fields: []
+    };
 
-        let embed = {
-            color: client.theme.color,
-            title: 'Command list for Songyy-bot',
-            url: 'https://github.com/gabrielpetry/songyy-bot',
-            description: 'All available commands',
-            footer: {
-               text: 'Songyy-Bot'
-            },
-                fields: []
-        };
-
-        if (playlists) {
-            playlists.map(playlist => {
-                embed.fields.push({
-                    name: `**${playlist.name}**`,
-                    value: `has ${playlist.SongsCount} songs`
-                });
-            });
-          return await message.channel.send({ embed });
-        }
-        return await message.channel.send("Error loading playlists");
-    },
-
-    help: {
-        name: "playlists",
-        description: "Show all playlists and the amount of songs in each",
-        usage: "playlists"
+    if (playlists) {
+      playlists.map(playlist => {
+        embed.fields.push({
+          name: `**${playlist.name}**`,
+          value: `has ${playlist.songs.length} songs`
+        });
+      });
+      return await message.channel.send({ embed });
     }
+    return await message.channel.send("Error loading playlists");
+  },
+
+  help: {
+    name: "playlists",
+    description: "Show all playlists and the amount of songs in each",
+    usage: "playlists"
+  }
 };
