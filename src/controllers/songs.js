870037f17f741ba628de9bef612895c873a26f69
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 14:35:32 -0300

    changed endpoint

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -3,8 +3,8 @@ const { api } = require("../services/api");
 module.exports = {
   getSongs: async (playlist = "") => {
     if (!playlist) return [];
-    let result = await api.get(`/songs/${playlist}`);
+    let result = await api.get(`/playlists/${playlist}`);
     if (!result) return [];
-    return result.data.songs;
+    return result.data[0].songs;
   }
 };
