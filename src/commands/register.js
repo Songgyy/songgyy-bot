9a9e1e7a56934be3d14e9e83af931f5c155b58d7
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 14:36:23 -0300

    prettier

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
-const guild = require('./../controllers/guild');
+const guild = require("./../controllers/guild");
 
 module.exports = {
+  run: async (client, message, args) => {
+    const { id, name } = message.guild;
 
-    run: async (client, message, args) => {
-        const { id, name } = message.guild;
+    const result = guild.registerGuild(id, name);
 
-        const result = guild.registerGuild(id, name);
+    if (result.error)
+      return await message.channel.send("Error registering this guild");
+    return await message.channel.send("Guild registered");
+  },
 
-        if (result.error)
-            return await message.channel.send("Error registering this guild");
-        return await message.channel.send("Guild registered")
-    },
-
-    help: {
-        name: "register",
-        description: "Register this guild to the main server, allowing to have playlists.",
-        usage: "register"
-    }
+  help: {
+    name: "register",
+    description:
+      "Register this guild to the main server, allowing to have playlists.",
+    usage: "register"
+  }
 };
