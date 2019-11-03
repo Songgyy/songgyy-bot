c65ca841fb3b59b48eb704a37ed40c143e9574fa
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 14:49:18 -0300

    correct endpoint

--- src/controllers/playlists.js
+++ src/controllers/playlists.js
@@ -2,10 +2,9 @@ const { api } = require("../services/api");
 
 module.exports = {
   getPlaylists: async () => {
-    let result = await api
-      .get("/playlists/songs")
-      .catch(err => console.log(err));
+    let result = await api.get("/playlists").catch(err => console.log(err));
     if (!result) return [];
+    console.log(result.data);
     return result.data.playlists;
   }
 };
