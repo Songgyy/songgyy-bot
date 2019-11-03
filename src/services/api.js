9edd9d0880e16f3df88961f5eb91d5ba115951b3
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 14:18:34 -0300

    change base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
-const config = require('./../config.json');
+const axios = require("axios");
+const config = require("./../config.json");
 
 module.exports = {
   api: axios.create({
-    baseURL: 'https://gabrielpetry.com.br/songyy-api',
+    baseURL: "http://gabrielpetry.com.br:21142",
     headers: {
       Authorization: `Bearer ${config.api_token}`
     }
-  }),
+  })
 };
