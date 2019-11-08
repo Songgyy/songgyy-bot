0a8d27a6491c1247b9b2f9f39d3f24bf0994a50c
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-08 06:52:22 -0300

    Register should send a username

--- src/commands/register.js
+++ src/commands/register.js
@@ -4,8 +4,14 @@ const guild = require("./../controllers/guild");
 module.exports = {
   run: async (client, message, args) => {
     const { id, name } = message.guild;
+    const username = args[0]
 
-    const result = guild.registerGuild(id, name);
+    if (!username) {
+        return await message.channel.send(`Please, provide a username.`)
+    }
+
+
+    const result = guild.registerGuild(id, name, username);
 
     if (result.error)
       return await message.channel.send("Error registering this guild");
--- src/controllers/guild.js
+++ src/controllers/guild.js
@@ -1,10 +1,11 @@
 const { api } = require('../services/api');
 
 module.exports = {
-  registerGuild: async (id, name) => {
+  registerGuild: async (id, name, username) => {
     let result = await api.post('/guilds', {
       id,
-      name
+      name,
+      username
     }).catch(err => console.log(err));
     if (!result)
       return [];
