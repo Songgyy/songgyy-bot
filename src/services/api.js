7f10f7fdcd46f5f9dd50e365f8682b6c001b2c6c
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-08 07:52:15 -0300

    docker host

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ const config = require("./../config.json");
 module.exports = {
   api: axios.create({
     baseURL: "http://gabrielpetry.com.br:21142",
+    // baseURL: "http://172.18.0.1:3333/",
     headers: {
       Authorization: `Bearer ${config.api_token}`
     }
