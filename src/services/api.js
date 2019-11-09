c6e149ecf9b75f19f13250a991f941976eae19a2
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-09 16:12:49 -0300

    Upgraded to use ssl connection

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const config = require("./../config.json");
 
 module.exports = {
   api: axios.create({
-    baseURL: "http://gabrielpetry.com.br:21142",
+    baseURL: 'https://gabrielpetry.com.br/v2/',
     // baseURL: "http://172.18.0.1:3333/",
     headers: {
       Authorization: `Bearer ${config.api_token}`
