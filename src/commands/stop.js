b2492701f6e12e9927d282cd79dd5495f0a510a0
Gabriel Petry <gabrielpetry@outlook.com>
2019-12-28 22:34:04 -0300

    validade if dispatcher exists

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -5,7 +5,10 @@ module.exports = {
     run: async (client, message, args) => {
         const sq = client.Queue.get(message.guild.id);
         sq.songs = [];
-        sq.connection.dispatcher.end();
+        if (sq.connection.dispatcher)
+            sq.connection.dispatcher.end();
+        else
+            sq.textChannel.send(`Something bad happend stopping this queue`);
     },
 
     help: {
